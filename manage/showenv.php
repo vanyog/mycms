b542578346aa809bb0ec9e32d1025708ef830229
vanyog <info@vanyog.com>
2012-12-10 00:40:03 +0200

    showenv.php fie was added

---
+++ manage/showenv.php
@@ -0,0 +1,25 @@
+<!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN" "http://www.w3.org/TR/html4/loose.dtd">
+<HTML>
+
+<HEAD>
+  <TITLE></TITLE>
+  <META HTTP-EQUIV="Content-Type" CONTENT="text/html; charset=windows-1251">
+</HEAD>
+
+<BODY>
+<script language="php">
+
+//Показва се съдържанието на $_SERVER променливата
+
+$k=array_keys($_SERVER);
+sort($k);
+echo '<table border=0>';
+foreach ($k as $k0){
+ echo '<tr><td>$_SERVER[\''.$k0.'\']</td><td nowrap> = '.$_SERVER[$k0].'</td></tr>'."\n";
+}
+echo '</table>';
+phpinfo();
+
+</script>
+</BODY>
+</HTML>
