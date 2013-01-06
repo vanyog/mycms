e4b0ae410858281227c98441d60d6649120c97fa
Vanyo Georgiev <info@vanyog.com>
2013-01-06 09:25:21 +0200

    A bug under windows was corrected

--- manage/tables.sql
+++ manage/tables.sql
@@ -1,25 +1,3 @@
--- phpMyAdmin SQL Dump
--- version 3.4.3.2
--- http://www.phpmyadmin.net
---
--- Хост: localhost
--- Време на генериране:  2 яну 2013 в 09:56
--- Версия на сървъра: 5.5.15
--- Версия на PHP: 5.3.8
-
-SET SQL_MODE="NO_AUTO_VALUE_ON_ZERO";
-SET time_zone = "+00:00";
-
-
-/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
-/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
-/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
-/*!40101 SET NAMES utf8 */;
-
---
--- База данни: `proba`
---
-
 -- --------------------------------------------------------
 
 --
@@ -167,6 +145,3 @@ CREATE TABLE IF NOT EXISTS `visit_history` (
   KEY `count` (`count`)
 ) ENGINE=MyISAM DEFAULT CHARSET=utf8 AUTO_INCREMENT=1 ;
 
-/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
-/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
-/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
