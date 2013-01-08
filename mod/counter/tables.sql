835554e5646420f930ea5958a001078b9af5dedc
vanyog <info@vanyog.com>
2013-01-09 00:13:36 +0200

    Field order in mod

--- mod/counter/tables.sql
+++ mod/counter/tables.sql
@@ -1,16 +1,16 @@
 CREATE TABLE IF NOT EXISTS `mod_counter` (
   `ID` int(11) NOT NULL AUTO_INCREMENT,
+  `IP` varchar(15) NOT NULL,
+  `date_time` datetime NOT NULL,
   `url` varchar(100) CHARACTER SET utf8 NOT NULL,
   `page` varchar(255) NOT NULL,
   `referrer` varchar(255) NOT NULL,
   `agent` varchar(255) NOT NULL,
-  `date_time` datetime NOT NULL,
-  `IP` varchar(15) NOT NULL,
   PRIMARY KEY (`ID`),
+  KEY `IP` (`IP`),
+  KEY `date_time` (`date_time`),
   KEY `url` (`url`),
   KEY `page` (`page`),
   KEY `referrer` (`referrer`),
-  KEY `agent` (`agent`),
-  KEY `date_time` (`date_time`),
-  KEY `IP` (`IP`)
+  KEY `agent` (`agent`)
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1 AUTO_INCREMENT=1 ;
