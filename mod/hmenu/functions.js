3cb2e584318e80abccc750b9e32166a8ba302d40
vanyog <info@vanyog.com>
2015-10-27 08:11:04 +0200

    Improvement

--- mod/hmenu/functions.js
+++ mod/hmenu/functions.js
@@ -2,19 +2,9 @@ var last_hlayer = 1;
 var last_hitem = null;
 
 function getHTop(e){
-<<<<<<< HEAD
-var r = e.getBoundingClientRect();
-//alert(r.top);
-//return r.top;
 var t = 0;
 do {
   t += e.offsetTop;
-//  alert(e.id + " "+e.offsetTop + " "+e.style.position );
-=======
-var t = 0;
-do {
-  t += e.offsetTop;
->>>>>>> a4315946ef3842c73660e8cee97aa86450811558
   e = e.offsetParent;
 } while (e);
 return t;
@@ -27,18 +17,8 @@ var el = document.getElementById("HLayer"+id);
 if (el){
   var s = el.style;
   s.visibility = "visible";
-<<<<<<< HEAD
-//  s.top = (getHTop(e)+e.offsetHeight-2)+"px";//(e.offsetTop+161)+"px";
-  s.top = (getHTop(e) + e.offsetHeight - 2) + "px";
-//  s.top = "138px";
-  var w;
-  w = Math.round(e.getBoundingClientRect().left) + "px";
-//  w = Math.round(e.offsetLeft) + "px";
-  s.left = w;
-=======
   s.top = (getHTop(e) + e.offsetHeight - 2) + "px";
   s.left = Math.round(e.getBoundingClientRect().left) + "px";
->>>>>>> a4315946ef3842c73660e8cee97aa86450811558
   last_hlayer = id;
 }
 if (last_hitem && (last_hitem!=e)){
