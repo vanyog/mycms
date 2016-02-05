2a19e2acb270607d45fb95f550941790c50ab7d2
vanyog <info@vanyog.com>
2016-02-05 10:10:10 +0200

    New module sitemap

---
+++ mod/sitemap/f_sitemap.php
@@ -0,0 +1,97 @@
+<?php
+/*
+MyCMS - a simple Content Management System
+Copyright (C) 2013  Vanyo Georgiev <info@vanyog.com>
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
+// Функцията sitemap($i) генерира html код за показване карта на сайта.
+// $i е номера на менюто на страницата, от която се разклонява картата.
+// Функцията извиква сама себе си (рекурсия) за да получи карти на подменютата.
+
+include_once($idir.'lib/f_db_table_field.php');
+
+$page_passed = array(); // Номера на менюта, които вече са обработени,
+                        // използва са за да не се получи зацикляне
+$map_lavel = 0; // Ниво на рекурсията
+$i_root    = 0; // Номер на входното меню
+
+function sitemap($a){
+global $page_passed, $map_lavel, $i_root;
+$page_passed = array();
+$map_lavel = 0;
+$i_root    = 0;
+$ar = explode('|',$a);
+$id = 'site_map';
+if (isset($ar[1])) $id = $ar[1];
+return '<div id="'.$id.'">'."\n".sitemap_rec($ar[0])."
+<p class=\"clear\"></p></div>\n";
+}
+
+function sitemap_rec($i){
+global $pth, $page_passed, $map_lavel, $i_root, $ind_fl;
+
+$page_passed[] = $i;
+
+$count = 1; // Номер на поредната връзка
+
+$rz = "\n"; 
+$ind = (40*$map_lavel).'px';
+if (!$i_root) $i_root = $i;
+
+// Извличат се данни за хипервръзките от меню $i
+$mi = db_select_m('*', 'menu_items', "`group`=$i ORDER BY `place`");
+
+// Прочита се номера на главната страница на менюто
+$index = db_table_field('index_page','menu_tree',"`group`=$i");
+
+// Цикъл за обработка на всяка хипервръзка от менюто $i
+foreach($mi as $m){
+  $rz .= "<div id=\"sml_$map_lavel"."_$count\">";
+  
+  // Съставяне на хипервръзка за картата
+  $pid = 1*$m['link'];
+  if (($i==$i_root)||($pid!=$index))
+  { 
+    $lk = $m['link'];
+    if ($pid) $lk = $ind_fl.'?pid='.$pid;
+//    $rz .= '<span style="padding-left:'.$ind.'"><a href="'.$lk.'">'.translate($m['name']).'</a></span><br>'."\n";
+    $rz .= '<a href="'.$lk.'">'.translate($m['name']).'</a><br>'."\n";
+    $count++;
+  }
+
+  if ($pid) { // Обработва се ако е страница с номер от настоящата CMS
+
+    // Извличат се данните за страницата
+    $p = db_select_1('*','pages','`ID`='.$pid);
+
+    if ($p['menu_group']!=$i){ // Ако е страница от друго меню
+      // Рекурсивно извикване за получаване карта на подменюто
+      if (!in_array($p['menu_group'],$page_passed)){
+        $map_lavel++;
+        $rz .= sitemap_rec($p['menu_group']);
+        $map_lavel--;
+      }
+    }
+  }
+  $rz .= '</div>';
+
+} // Край на цикъла за обработка на всяка хипервръзка от менюто $i
+
+return $rz."\n";
+
+}
+
+?>
