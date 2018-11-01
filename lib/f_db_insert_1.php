37ef38e36ea96c405241fdfd77d8297736d7d939
vanyog <info@vanyog.com>
2018-11-01 10:05:22 +0200

    Improvements

--- lib/f_db_insert_1.php
+++ lib/f_db_insert_1.php
@@ -33,7 +33,7 @@ foreach($d as $n=>$v){
   else $q .= "`$n`='".addslashes($v)."',";
 }
 $q = substr($q,0,strlen($q)-1).";";
-if ($y) return $q;
+if ($y) return "$q<br>\n";
 else{
  mysqli_query($db_link,$q);
  $db_req_count++;
